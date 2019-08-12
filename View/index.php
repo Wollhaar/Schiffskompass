70aef8adeb2201d904d4c0c459fc6aad50aef1c5
David Goraj <d.goraj@exinit.de>
2019-08-13 00:37:29 +0200

    alternative Kursänderung mit ajax geschrieben.

--- View/index.php
+++ View/index.php
@@ -11,21 +11,42 @@ $gradZahl = $kurs->getGrad();
 <header style="width:200px;text-align:center;color:red;background:beige;"><h2>Der Schiffskompass</h2></header>
 <div class="wrapper">
     <div class="steuern">
-        <form action="index.php" method="get">
-            <label for="steuergrad">Steuere das Ruder</label>
-            <input id="steuergrad" name="grad" placeholder="Gradzahl eingeben">
-            <button type="submit">&Auml;ndere Kurs</button>
-        </form>
+        <label for="steuergrad">Steuere das Ruder</label>
+        <input id="steuergrad" name="grad" placeholder="Gradzahl eingeben">
     </div>
+    <div class="kurs-aenderung">
     <?php if($gradZahl != $alt): ?>
-    <div class="grad">
         <span>Ruder wurde um <?php echo $kurs->getGrad(); ?> gedreht.</span>
-    </div>
     <?php endif; ?>
+    </div>
 </div>
 
 <script type="text/javascript">
+    $(document).ready(function() {
+      $('input#steuergrad').onkeyup(function () {
+        var input = [];
+        input[$(this).attr('name')] = $(this).val();
+        send_post(input);
+      });
+    });
 
+    function send_post(value){
+      $.post({'grad': value}, function (succeed, status) {
+        var data = succeed.split('\\;');
+        if(status){
+          for(var i = 0; data.length > i; i++) {
+            if(data[i].search('TRUE') !== -1){
+              neuen_kurs_anzeigen(data[i+1])
+            }
+          }
+        }
+      });
+    }
+
+    function neuen_kurs_anzeigen(data){
+      var output = '<p>Ruder wurde um ' + data[1] + ' gedreht.</p>';
+      $('.kurs-aenderung').append(output);
+    }
 </script>
 </body>
 </html>
